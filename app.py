521b89b5bd0ce0d2be9268c431cf58a7aee9ef20
gunalan-samsil <105532119+gunalan-samsil@users.noreply.github.com>
2022-05-21 08:22:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,44 @@ client = Cloudant(user, password, url=url, connect=True)
 app = Flask(__name__)
 app.config['SECRET_KEY']="QWERTYUIOPASDFGHJKLZXCVBNM"
 
+#Machine Learning Model
+dataset = pd.read_excel("https://github.com/gunalan-samsil/adaptiv-effort-estimator/blob/master/dataset_integration_v2.xlsx?raw=true",skip_header=1)
+Y=dataset.iloc[:, 13:]
+X=dataset.iloc[:,1:13]
+header=list(X)
+imputer = Imputer()
+dataset = imputer.fit_transform(X)
+lr=LinearRegression()
+model_int=lr.fit(X,Y)
+print(model_int.score(X,Y))
+filename = 'finalized_model.sav'
+pickle.dump(model, open(filename, 'wb'))
+
+def intRegression(req):
+    
+    #Data Processing
+    val=[]
+    result=req.get("result")
+    contexts=result.get("contexts")
+    print(contexts[0])
+    parameters=contexts[0].get("parameters")
+    for i in header:
+        str=parameters.get(i)
+        print("%s %s " %(i,str))
+        val.append(str)
+    ds=pd.DataFrame(val).T
+    print(ds)
+
+    #Prediction
+    loaded_model = pickle.load(open(filename, 'rb'))
+    op_lrt=loaded_model.predict(ds)
+    print(op_lrt)
+    
+    op=[5]
+    op=[round(op_lrt[0][0],2),model_int.score(X,Y)]
+    print(op)
+    return op
+
 @app.route('/webhook',methods=['POST'])
 def webhook():
     url="https://nwave-ideabot-flask-webhook-p.herokuapp.com/storedata"
@@ -102,40 +140,62 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
    
-def intRegression(req):
-    #Machine Learning Model
-    dataset = pd.read_excel("https://github.com/s-gunalan/nWave-Flask-Demo/blob/master/dataset_integration_v2.xlsx?raw=true",skip_header=1)
-    #dataset=pd.read_excel("D:/Guna/POCs/ML/nWave_effort/dataset_integration.xlsx",skip_header=1)
-    Y=dataset.iloc[:, 13:]
-    X=dataset.iloc[:,1:13]
-    header=list(X)
-    imputer = Imputer()
-    dataset = imputer.fit_transform(X)
-    lr=LinearRegression()
-    model_int=lr.fit(X,Y)
-    print(model_int.score(X,Y))
-    #Data Processing
-    val=[]
-    result=req.get("result")
-    contexts=result.get("contexts")
-    print(contexts[0])
-    parameters=contexts[0].get("parameters")
-    for i in header:
-        str=parameters.get(i)
-        print("%s %s " %(i,str))
-        val.append(str)
-    ds=pd.DataFrame(val).T
-    print(ds)
+@app.route('/train',methods=['POST'])
+def retrain():
+    output={}
+    try:
+        req=request.get_json(silent=True,force=True)
+        sessionId=req.get("sessionId")
+        result=req.get("result")
+        contexts=result.get("contexts")
+        par=contexts[2].get("parameters")
+        product=par.get("product")
+        srcprotocol=par.get("srcprotocol")
+        srcformat=par.get("srcmsgformat")
+        targetmsgformat=par.get("targetmsgformat")
+        targetprotocol=par.get("targetprotocol")
+        associateId=par.get("number-integer")
+        operationcount=par.get("operationcount")
+        intdataformat=par.get("int-dataformat")
+        Interfacetype=par.get("Interface-type")
+        rulecount=par.get("rulecount")
+        msgfieldcount=par.get("msgfieldcount")
+        exposedasapi=par.get("exposed-as-api")
+        newexisting=par.get("new-existing")
+        dispproduct=par.get("product.original")
+        dispsrcF=par.get("srcmsgformat.original")
+        dispsrcP=par.get("srcprotocol.original")
+        disptargetF=par.get("targetmsgformat.original")
+        disptargetP=par.get("targetprotocol.original")
+        regop=intRegression(req)
+        weightage=regop[0]
+        
+        op={'sessionId':sessionId,
+            'weightage':weightage,
+            'product':product,
+            'srcprotocol':srcprotocol,
+            'srcmsgformat':srcformat,
+            'targetmsgformat':targetmsgformat,
+            'targetprotocol':targetprotocol,
+            'associateId':associateId,
+            'operationcount':operationcount, 
+            'int-dataformat':intdataformat,
+            'Interface-type': Interfacetype,
+            'rulecount':rulecount,
+            'msgfieldcount':msgfieldcount,
+            'exposed-as-api':exposedasapi,
+            'new-existing':newexisting,
+            'disp-product':dispproduct,
+            'disp-srcF':dispsrcF,
+            'disp-srcP':dispsrcP,
+            'disp-targetF':disptargetF,
+            'disp-targetP':disptargetP,
+            'feedback':"Not Given",
+            'admin-flag':0
+           }
+        df = pd.DataFrame(op)
+        pd.to_excel("https://github.com/gunalan-samsil/adaptiv-effort-estimator/blob/master/dataset_integration_v2.xlsx?raw=true",skip_header=1)
 
-    #Prediction
-    op_lrt=lr.predict(ds)
-    print(op_lrt)
-    
-    op=[5]
-    op=[round(op_lrt[0][0],2),model_int.score(X,Y)]
-    print(op)
-    return op
-    
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
        	app.run(host='0.0.0.0', port=int(port), use_reloader=True, debug=True)
